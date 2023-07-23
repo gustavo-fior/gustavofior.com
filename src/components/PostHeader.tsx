8cff77cc317cf57c897b85e06dea38580061b928
Gustavo <gustavo.fior@outlook.com>
2023-07-23 11:36:02 -0300

    fix emoji size in mobile

--- src/components/PostHeader.tsx
+++ src/components/PostHeader.tsx
@@ -13,7 +13,7 @@ const PostHeader = ({
 }: PostHeaderProps) => {
   return (
       <div className="pb-4">
-        <h1 className="pb-6 text-4xl sm:text-5xl font-bold md:text-6xl lg:text-7xl">{emoji}</h1>
+        <h1 className="pb-6 text-5xl font-bold md:text-6xl lg:text-7xl">{emoji}</h1>
         <h1 className="pb-4 text-3xl sm:text-4xl font-bold md:text-5xl">{title}</h1>
         <h3 className="pb-4 text-slate-400 md:text-lg">
           {date} • <span className="italic">{readTime}</span>
