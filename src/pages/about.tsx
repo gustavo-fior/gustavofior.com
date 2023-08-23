14c454fc1cefd5eb85b9a0d164532c4abb47875f
Gustavo <gustavo.fior@sbcash.com.br>
2023-08-23 15:18:16 -0300

    add animation to /about (need to make it cooler, looks just like any other blogpost)

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,93 +3,119 @@ import Head from "next/head";
 import ContentWrapper from "~/components/ContentWrapper";
 import Header from "~/components/Header";
 import LinkText from "~/components/md/LinkText";
+import { type Variants, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const About: NextPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const variants: Variants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  };
+
+  useEffect(() => {
+    setIsOpen(true);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Gustavo&apos;s home</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="https://www.gustavofior.com/api/og" />
+        <meta
+          property="og:image"
+          content="https://www.gustavofior.com/api/og"
+        />
       </Head>
       <Header />
       <ContentWrapper>
-        <h1 className="pb-6 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
-          🙋🏼‍♂️
-        </h1>
-        <h1 className="pb-4 text-4xl font-bold md:text-5xl">About me</h1>
-        <h3 className="pb-4 text-slate-400 md:text-lg">
-          June 12, 2023 • <span className="italic">2 min read</span>
-        </h3>
-        <div className="pt-4 text-lg text-slate-300">
-          <p className="pb-4">
-            Hello there! My name is Gustavo and I&apos;m a software engineer
-            from Brazil. I really like coding, surfing, reading books, and
-            learning new things.
+        <motion.div
+          initial={false}
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+        >
+          <h1 className="pb-6 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
+            🙋🏼‍♂️
+          </h1>
+          <h1 className="pb-4 text-4xl font-bold md:text-5xl">About me</h1>
+          <h3 className="pb-4 text-slate-400 md:text-lg">
+            June 12, 2023 • <span className="italic">2 min read</span>
+          </h3>
+          <div className="pt-4 text-lg text-slate-300">
+            <p className="pb-4">
+              Hello there! My name is Gustavo and I&apos;m a software engineer
+              from Brazil. I really like coding, surfing, reading books, and
+              learning new things.
+              <br />
+              <br />
+              I&apos;m currently working as a backend software engineer at a
+              fintech in Curitiba called{" "}
+              <LinkText href="https://bancosb.com.br">SB Cash</LinkText>. This
+              is the tech stack I use on a daily basis:
+            </p>
+            <ul className="list-disc pl-8">
+              <li>Java ☕</li>
+              <li>Spring</li>
+              <li>
+                Kafka (
+                <LinkText href="https://developer.confluent.io/learn/kraft/">
+                  KRaft
+                </LinkText>
+                )
+              </li>
+              <li>Docker</li>
+              <li>PostgreSQL</li>
+              <li>MongoDB</li>
+              <li>AWS</li>
+            </ul>
             <br />
+            <p>
+              Like I said, I love to learn new things, so right now I&apos;m
+              studying Next.js (what I&apos;m using to build this website),
+              TypeScript, and <LinkText href="https://expo.dev">Expo</LinkText>.
+            </p>
             <br />
-            I&apos;m currently working as a backend software engineer at a
-            fintech in Curitiba called{" "}
-            <LinkText href="https://bancosb.com.br">SB Cash</LinkText>. This is
-            the tech stack I use on a daily basis:
-          </p>
-          <ul className="list-disc pl-8">
-            <li>Java ☕</li>
-            <li>Spring</li>
-            <li>
-              Kafka (
-              <LinkText href="https://developer.confluent.io/learn/kraft/">
-                KRaft
-              </LinkText>
-              )
-            </li>
-            <li>Docker</li>
-            <li>PostgreSQL</li>
-            <li>MongoDB</li>
-            <li>AWS</li>
-          </ul>
-          <br />
-          <p>
-            Like I said, I love to learn new things, so right now I&apos;m
-            studying Next.js (what I&apos;m using to build this website),
-            TypeScript, and <LinkText href="https://expo.dev">Expo</LinkText>.
-          </p>
-          <br />
-          <p>
-            Besides that, I&apos;m also co-founder of a startup called{" "}
-            <LinkText href="https://getmind.app">Mind</LinkText>, where me and
-            my friend <LinkText href="https://abdulhdr.com">Abdul</LinkText> are
-            building an app to make going to therapy a lot easier.
-          </p>
-          <br />
-          <p>
-            I keep a record of{" "}
-            <LinkText href="https://gustavofior.notion.site/Cool-Stuff-f4f54a6fe8fe4bc1bc34c588966fb631?pvs=4">
-              cool stuff
-            </LinkText>{" "}
-            I see on the internet, and you can pretty much tell what I&apos;m
-            interested in by looking at it.
-          </p>
-          <br />
-          <p>
-            That&apos;s it! If you want to talk to me,{" "}
-            <LinkText href="https://cal.com/gustavo-fior">schedule</LinkText> a
-            meeting anytime or reach me out on the links in the top right!
-          </p>
-          <br />
+            <p>
+              Besides that, I&apos;m also co-founder of a startup called{" "}
+              <LinkText href="https://getmind.app">Mind</LinkText>, where me and
+              my friend <LinkText href="https://abdulhdr.com">Abdul</LinkText>{" "}
+              are building an app to make going to therapy a lot easier.
+            </p>
+            <br />
+            <p>
+              I keep a record of{" "}
+              <LinkText href="https://gustavofior.notion.site/Cool-Stuff-f4f54a6fe8fe4bc1bc34c588966fb631?pvs=4">
+                cool stuff
+              </LinkText>{" "}
+              I see on the internet, and you can pretty much tell what I&apos;m
+              interested in by looking at it.
+            </p>
+            <br />
+            <p>
+              That&apos;s it! If you want to talk to me,{" "}
+              <LinkText href="https://cal.com/gustavo-fior">schedule</LinkText>{" "}
+              a meeting anytime or reach me out on the links in the top right!
+            </p>
+            <br />
 
-          <div className="rounded-lg bg-white bg-opacity-20 px-6 py-4 drop-shadow-lg backdrop-blur-lg">
-            <p className="text-white">
-              The life of every human being is a journey towards oneself.
-            </p>
-            <p className="italic">
-              Herman Hesse,{" "}
-              <LinkText href="https://en.wikipedia.org/wiki/Demian">
-                Demian
-              </LinkText>
-            </p>
+            <div className="rounded-lg bg-white bg-opacity-20 px-6 py-4 drop-shadow-lg backdrop-blur-lg">
+              <p className="text-white">
+                The life of every human being is a journey towards oneself.
+              </p>
+              <p className="italic">
+                Herman Hesse,{" "}
+                <LinkText href="https://en.wikipedia.org/wiki/Demian">
+                  Demian
+                </LinkText>
+              </p>
+            </div>
           </div>
-        </div>
+        </motion.div>
       </ContentWrapper>
     </>
   );
