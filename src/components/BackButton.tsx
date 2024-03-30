f115fe89b7d448bff369703f7f703df3166b49e1
Gustavo <gustavo_fior@outlook.com>
2024-03-30 15:21:03 -0300

    i hate tailwindy

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,12 +1,11 @@
 import { RxArrowLeft } from "react-icons/rx";
-import { primaryOrange } from "~/utils/colors";
 
 const BackButton = () => {
   return (
     <div className="py-12">
 
       <RxArrowLeft
-        className={`cursor-pointer text-2xl transition duration-200 ease-in-out hover:text-[${primaryOrange}]`}
+        className={`cursor-pointer text-2xl transition duration-200 ease-in-out hover:text-[#e64100]`}
         onClick={() => window.history.back()}
       />
     </div>
