945c4d60fe7a85dcc689cfcbb0b53d0293dce6cc
Gustavo <gustavo.fior@sbcash.com.br>
2024-04-16 09:26:56 -0300

    fix

--- src/components/md/Quote.tsx
+++ src/components/md/Quote.tsx
@@ -7,7 +7,7 @@ interface QuoteProps {
 const Quote = ({ children }: QuoteProps) => {
   return (
     <div className="flex gap-4 py-4">
-      <div className={`w-0.5 bg-[${primaryOrange}]`} />
+      <div className={`w-0.5 bg-[#e64100]`} />
       <blockquote> {children} </blockquote>
     </div>
   );
