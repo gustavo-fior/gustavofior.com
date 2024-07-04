b946e54089e5a4fd0b6ac4b356a6cd6a12f193fe
Gustavo <gustavo_fior@outlook.com>
2024-07-03 21:08:43 -0300

    yolo

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
