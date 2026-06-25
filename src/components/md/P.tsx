2b3925fa6df8d1b7e9d12bfa3c606b5248c076d7
Gustavo <gustavo_fior@outlook.com>
2026-06-25 07:45:00 -0300

    Refactor P component to simplify return statement
    - Removed unnecessary parentheses in the return statement of the P component for cleaner code.

--- src/components/md/P.tsx
+++ src/components/md/P.tsx
@@ -5,11 +5,7 @@ interface PProps {
 }
 
 const P = ({ children }: PProps) => {
-  return (
-    <p className="text-pretty text-base leading-[1.8] text-neutral-600">
-      {children}
-    </p>
-  );
+  return <p className="text-base leading-[1.8] text-neutral-600">{children}</p>;
 };
 
 export default P;
